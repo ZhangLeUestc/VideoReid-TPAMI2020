9abfdd3ddc14f72cb6588c674e241d63cf7ba952
Zhang Le <zhangleuestc@gmail.com>
2020-02-19 14:47:28 +0800

    Add files via upload

---
+++ reid/train/__init__.py
@@ -0,0 +1,7 @@
+from .trainers import Trainer
+from .trainers import BinaryTrainer
+from .trainers import JointTrainer
+from .trainers import CRFTrainer
+from .trainers import NOCRFTrainer
+from .trainers import MULJOINTTrainer
+from .trainers import MULJOINT_MAN_Trainer
---
+++ reid/utils/__init__.py
@@ -0,0 +1,11 @@
+
+from __future__ import absolute_import
+import torch
+
+def to_torch(ndarray):
+    if type(ndarray).__module__ == 'numpy':
+        return torch.from_numpy(ndarray)
+    elif not torch.is_tensor(ndarray):
+        raise ValueError("Cannot convert {} to torch tensor"
+                         .format(type(ndarray)))
+    return ndarray
